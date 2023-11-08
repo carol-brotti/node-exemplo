a6fb7c5fe9c0052ad45da65dc8bb9f4725c3ea5e
CAROLINA ALENCAR DA COSTA BROTTI <carolina.acbrotti@senacsp.edu.br>
2023-11-08 14:56:31 -0300

    configuracao do ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import validator from 'validator'
+
+console.log(validator.isEmail ('teste@'))
+
+let nome : String = "João"
+let idade: Number = 18
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos gdryanvjhhasbdvj`)
+
+
